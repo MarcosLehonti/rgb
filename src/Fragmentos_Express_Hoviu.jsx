83c10b72b985e20851057d9e004399b59eecf14b
MLGM2001 <canonmauricio63@gmail.com>
2025-08-17 16:33:25 -0400

    fragmentos express de hoviu

--- src/Fragmentos_Express_Hoviu.jsx
+++ src/Fragmentos_Express_Hoviu.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 import './Styles/Fragmentos_Express_Hoviu.css'
 
 
-
-import img1 from '../src/assets/Fragmentos_Express_Hoviu/pagina1.jpg'
-import img2 from '../src/assets/Fragmentos_Express_Hoviu//pagina2.jpg'
-import img3 from '../src/assets/Fragmentos_Express_Hoviu//pagina3.jpg'
-import img4 from '../src/assets/Fragmentos_Express_Hoviu//pagina4.jpg'
+import portada from '../src/assets/Fragmentos_Express_Hoviu/portada.jpg'
+import division1 from '../src/assets/Fragmentos_Express_Hoviu/Separacion1.jpg'
+import img1 from '../src/assets/Fragmentos_Express_Hoviu/pagina11.jpg'
+import division2 from '../src/assets/Fragmentos_Express_Hoviu/Separacion2.jpg'
+import img2 from '../src/assets/Fragmentos_Express_Hoviu//pagina21.jpg'
+import division3 from '../src/assets/Fragmentos_Express_Hoviu/Separacion3.jpg'
+import img3 from '../src/assets/Fragmentos_Express_Hoviu//pagina31.jpg'
+import division4 from '../src/assets/Fragmentos_Express_Hoviu/Separacion4.jpg'
+import img4 from '../src/assets/Fragmentos_Express_Hoviu//pagina41.jpg'
 import Navbar from './Navbar'
 
 
-const images = [img1, img2, img3, img4]
+const images = [portada,division1,img1, division2,img2,division3, img3,division4, img4]
 
 function Fragmentos_Express_Hoviu() {
   const [current, setCurrent] = useState(0)
--- src/Hoviu.jsx
+++ src/Hoviu.jsx
@@ -3,7 +3,8 @@ import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import './Styles/Hoviu.css'
-import kangue from './assets/kangue.png'
+import Portada from './assets/Fragmentos_Express_Hoviu/portada.jpg'
+import kangue  from './assets/Fragmentos_Express_Hoviu/portada.jpg'
 import Navbar from './Navbar'
 
 function Hoviu() {
@@ -16,16 +17,18 @@ function Hoviu() {
       <div className="container">
             <div class="comic-card">
               <div class="comic-image">
-                <img src={kangue} alt="Portada del cómic" />
+                <img src={Portada} alt="Portada del cómic" />
               </div>
               <div class="comic-info">
-                <h2>Fragmentos Express Hoviu</h2>
-                <p>Esta es una breve descripción del cómic, puedes contar de qué trata o dejar un mensaje introductorio.</p>
+                <h2>Fragmentos Express de Hoviu</h2>
+                <p>Dicen que cada día es una oportunidad nueva… pero yo siento que cada día es más pesado que el anterior. 
+                  No hay instrucciones para sobrellevar el vacío, solo fragmentos. No sé en qué momento todo empezó a sentirse distinto. 
+                  Cada día parece igual, pero dentro de mí algo se rompe y se recompone en silencio.</p>
                 <button
                           className="btn btn-kangue"
                           onClick={() => navigate('/fragmentosexpresshoviu')}
                           >
-                          Saber más
+                          Leer
                       </button>  
               </div>
             </div>
